737ddea7d8a09c511b878d84547895b32c272c4f
Sumit Jana <sumitjana92@hotmail.com>
2019-08-07 02:38:16 +0530

    Add project files.

---
+++ MVC_Pagination/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MVC_Pagination.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Pagination.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? page)
+        {
+            var dummyItems = Enumerable.Range(1, 152).Select(x=>"My custom item"+x);
+            Pager pager = new Pager(dummyItems.Count(),page);
+            IndexViewModel viewModel = new IndexViewModel();
+            viewModel.Items = dummyItems.Skip((pager.CurrentPage - 1) * pager.PageSize).Take(pager.PageSize);
+            viewModel.pager = pager;            
+            return View(viewModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC_Pagination/Models/IndexViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Pagination.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<string> Items { get; set; }
+        public Pager pager { get; set; }
+    }
+
+    public class Pager
+    {
+        public int TotalItems { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalPages { get; private set; }
+        public int StartPage { get; private set; }
+        public int EndPage { get; private set; }
+        public Pager(int totalItems, int? page, int pageSize = 10)
+        {
+            int totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)pageSize);
+            var currentpage = page != null ? (int)page : 1;
+            var startpage = currentpage - 5;
+            var endpage = currentpage + 4;
+            if (startpage <= 0)
+            {
+                endpage -= (startpage - 1);
+                startpage = 1;
+            }
+            if (endpage > totalPages)
+            {
+                endpage = totalPages;
+                if (endpage > 10)
+                {
+                    startpage = endpage - 9;
+                }
+            }
+            TotalPages = totalPages;
+            CurrentPage = currentpage;
+            PageSize = pageSize;
+            TotalItems = totalItems;
+            StartPage = startpage;
+            EndPage = endpage;
+        }
+    }
+}
